53e15a536b0ff2b3f4b0549c174d59b5cc018e5e
Dev Sirohi <devsirohi000@gmail.com>
2026-04-28 23:39:49 +0530

    Started working on huffman-encoding-in-c project & installed Haskell

---
+++ huffman-encoding-in-c/error-codes.h
@@ -0,0 +1,10 @@
+#ifndef HUFFMAN_ENCODING_ERROR_CODES
+#define HUFFMAN_ENCODING_ERROR_CODES
+
+/* Process errors */
+#define INVALID_SCANF 101
+
+/* File management errors */
+#define INVALID_FILE_PATH 2001
+
+#endif
--- huffman-encoding-in-c/main.c
+++ huffman-encoding-in-c/main.c
@@ -1,27 +1,50 @@
+#include <../huffman-encoding-in-c/error-codes.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define STR_SIZE_SMALL 255
+void process_error(int error_code);
 
-int runApp()
+int run_app(void)
 {
-    int r_code = 0;
-    // Accept path to file
-    char* path = calloc(STR_SIZE_SMALL, sizeof(char));
-    fgets(path, STR_SIZE_SMALL, stdin);
+    printf("%s\n", "Huffman Encoding w. C");
+    printf("%s\n", "Initializing...");
 
-    FILE* file;
-    file = fopen(path, "r");
-    if (file == NULL)
+    printf("%s", "Enter path to the file you want to encode: ");
+
+    char *file_path = "";
+    if (scanf("%s", file_path) != 1)
     {
-        printf("Invalid file");
+        return INVALID_SCANF;
     }
 
-    return r_code;
+    FILE *fp = fopen(file_path, "r");
+    if (fp == NULL)
+    {
+        return INVALID_FILE_PATH;
+    }
 }
 
-int main()
+int main(void)
 {
-    runApp();
+    int result = run_app();
+    if (result > 0)
+    {
+        process_error(result);
+    }
+
     return 0;
+}
+
+void process_error(int error_code)
+{
+    switch (error_code)
+    {
+    case INVALID_SCANF:
+        printf("%s", "Incorrect input");
+        break;
+    default:
+        return;
+    }
+
+    return;
 }
