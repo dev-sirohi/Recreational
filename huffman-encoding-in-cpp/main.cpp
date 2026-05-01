e40495d6d8a8024ac234896115c9185245377e4e
Dev Sirohi <devsirohi000@gmail.com>
2026-05-01 18:21:58 +0530

    Huffman continued

--- huffman-encoding-in-cpp/main.cpp
+++ huffman-encoding-in-cpp/main.cpp
@@ -20,6 +20,9 @@ int main()
     {
         std::cerr << err << std::endl;
     }
+    catch (const std::exception &ex)
+    {
+    }
 
     return EXIT_SUCCESS;
 }
@@ -29,8 +32,8 @@ class HuffmanNode
   public:
     char character;
     int frequency;
-    HuffmanNode *leftPtr;
-    HuffmanNode *rightPtr;
+    HuffmanNode *left_ptr;
+    HuffmanNode *right_ptr;
 };
 
 struct
@@ -45,15 +48,17 @@ std::string huffman_encode(std::string text_to_encode)
 {
     if (text_to_encode.length() == 0)
     {
-        throw("Cannot encode text with length 0");
+        throw std::runtime_error("Cannot encode text with length 0");
     }
 
+    /* Create frequency map for each character */
     int frequency[256] = {0};
     for (char c : text_to_encode)
     {
         frequency[(int)c]++;
     }
 
+    /* Create nodes and push them to min priority_queue */
     std::priority_queue<HuffmanNode *, struct huffman_node_compare> priority_q;
     for (int i = 0; i < 256; i++)
     {
@@ -62,13 +67,15 @@ std::string huffman_encode(std::string text_to_encode)
             HuffmanNode *node = (HuffmanNode *)calloc(1, sizeof(HuffmanNode));
             node->character   = (char)i;
             node->frequency   = frequency[i];
-            node->leftPtr     = NULL;
-            node->rightPtr    = NULL;
+            node->left_ptr    = NULL;
+            node->right_ptr   = NULL;
             priority_q.push(node);
         }
     }
     free(frequency);
 
+    /* Pop two nodes & merge them and do this until only the final merged node remains in the
+     * priority queue */
     while (priority_q.size() > 1)
     {
         HuffmanNode *node1 = priority_q.top();
@@ -79,11 +86,42 @@ std::string huffman_encode(std::string text_to_encode)
         HuffmanNode *merged_node = (HuffmanNode *)calloc(1, sizeof(HuffmanNode));
         merged_node->character   = '\0';
         merged_node->frequency   = node1->frequency + node2->frequency;
-        merged_node->leftPtr     = node1;
-        merged_node->rightPtr    = node2;
+        merged_node->left_ptr    = node1;
+        merged_node->right_ptr   = node2;
 
         priority_q.push(merged_node);
     }
 
+    /* Now, traverse this binare tree and keep appending a path to assign to leaf nodes */
     HuffmanNode *root = priority_q.top();
+    std::vector<char> path(0);
+    std::queue<std::pair<HuffmanNode *, char> *> node_q;
+    std::pair<HuffmanNode *, char> *root_pair;
+    root_pair->first  = root;
+    root_pair->second = '\0';
+    node_q.emplace(root_pair);
+    while (node_q.size() > 0)
+    {
+        HuffmanNode *curr_node = node_q.front()->first;
+        char encoding_char     = node_q.front()->second;
+        node_q.pop();
+        if (curr_node->left_ptr != nullptr)
+        {
+            std::pair<HuffmanNode *, char> *left_pair;
+            left_pair->first  = curr_node->left_ptr;
+            left_pair->second = '0';
+            node_q.emplace(left_pair);
+        }
+        if (curr_node->right_ptr != nullptr)
+        {
+            std::pair<HuffmanNode *, char> *right_pair;
+            right_pair->first  = curr_node->right_ptr;
+            right_pair->second = '1';
+            node_q.emplace(right_pair);
+        }
+        if (encoding_char != '\0')
+        {
+            path.push_back(encoding_char);
+        }
+    }
 }
