2ec8330458705bc614e99269d7d55f12079129be
Dev Sirohi <devsirohi000@gmail.com>
2026-05-04 18:22:11 +0530

    fix nested repo issue

---
+++ raylib/flappy_bird/globals.cpp
@@ -0,0 +1,9 @@
+int fn_get_window_width(void)
+{
+    return 800;
+}
+
+int fn_get_window_height(void)
+{
+    return 450;
+}
---
+++ raylib/flappy_bird/globals.h
@@ -0,0 +1,4 @@
+#pragma once
+
+const int fn_get_window_width(void);
+const int fn_get_window_height(void);
